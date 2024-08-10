95afa8bd06707a7a5fc4a4bae9f25d372d54da07
wild_duck <wezonaser50@gmail.com>
2024-08-10 18:42:39 +0300

    feat: init after bat man

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,33 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
 
 import path from 'path'
-
+import { devDependencies } from './package.json'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, './libs/src/'),
+            '@': path.resolve(__dirname, './lib'),
         },
     },
     build: {
         lib: {
-            entry: './duck-ui/index.ts',
-            name: '@ahmedayob/duck-ui',
+            entry: './lib/index.ts',
+            name: 'vite-react-ts-button',
             fileName: (format) => `duck-ui.${format}.js`,
             formats: ['cjs', 'es'],
         },
         rollupOptions: {
-            external: ['react', 'react-dom'],
+            external: [...Object.keys(devDependencies)],
         },
         sourcemap: true,
         emptyOutDir: true,
     },
     test: {
         globals: true,
+        environment: 'jsdom',
+        setupFiles: './setupTests.ts',
     },
     plugins: [dts()],
 })
