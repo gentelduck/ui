65cfaa725fc277b8e8c9fe78a865095a47cc2877
wild_duck <wezonaser50@gmail.com>
2024-08-11 13:01:21 +0300

    chore: package.json

---
+++ tsup.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+    entry: ['./lib/index.ts'],
+    format: ['cjs', 'esm'], // Corresponds to CommonJS and ES module formats
+    outDir: 'dist',
+    sourcemap: false, // Disable sourcemaps
+    clean: true, // Clean the output directory before each build
+    dts: true, // Generate type definitions
+    external: [
+        // Exclude dependencies to avoid bundling them
+        ...Object.keys(require('./package.json').devDependencies || {}),
+        ...Object.keys(require('./package.json').peerDependencies || {}),
+        '**/*.stories.tsx', // Exclude stories files
+        '**/*.mdx', // Exclude MDX files if necessary
+        '**/*.public/**', // Exclude files in public directories if necessary
+    ],
+    minify: false, // Set to true to enable minification
+})
--- vite.config.ts
+++ vite.config.ts
@@ -7,40 +7,40 @@ import { devDependencies, peerDependencies } from './package.json'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './lib'),
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './lib'),
+        },
     },
-  },
-  build: {
-    lib: {
-      entry: './lib/index.ts',
-      name: 'vite-react-ts-button',
-      fileName: (format) => `duck-ui.${format}.js`,
-      formats: ['cjs', 'es'],
+    build: {
+        lib: {
+            entry: './lib/index.ts',
+            name: 'vite-react-ts-button',
+            fileName: (format) => `duck-ui.${format}.js`,
+            formats: ['cjs', 'es'],
+        },
+        rollupOptions: {
+            external: [
+                ...Object.keys(devDependencies),
+                ...Object.keys(peerDependencies),
+                './lib/**/*.stories.tsx',
+                './public/**',
+            ],
+        },
+        sourcemap: false,
+        emptyOutDir: true,
     },
-    rollupOptions: {
-      external: [
-        ...Object.keys(devDependencies),
-        ...Object.keys(peerDependencies),
-        './lib/**/*.stories.tsx',
-        './public/**',
-      ],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './setupTests.ts',
     },
-    sourcemap: false,
-    emptyOutDir: false,
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './setupTests.ts',
-  },
-  plugins: [
-    dts(),
-    TanStackRouterVite({
-      experimental: {
-        enableCodeSplitting: true,
-      },
-    }),
-  ],
+    plugins: [
+        dts(),
+        TanStackRouterVite({
+            experimental: {
+                enableCodeSplitting: true,
+            },
+        }),
+    ],
 })
