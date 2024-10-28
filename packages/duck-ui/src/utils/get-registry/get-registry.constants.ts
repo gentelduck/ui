60c0696919fe05f3ace26eaf396b034c4a7a7dbf
wildduck <wezonaser50@gmail.com>
2024-10-28 10:07:57 +0300

    feat: refactoring the registry with new way

---
+++ packages/duck-ui/src/utils/get-registry/get-registry.constants.ts
@@ -0,0 +1,32 @@
+export const error_messages: { [key: number]: string } = {
+  400: 'Bad request',
+  401: 'Unauthorized',
+  403: 'Forbidden',
+  404: 'Not found',
+  500: 'Internal server error'
+}
+
+export async function get_registry_base_colors() {
+  return [
+    {
+      name: 'neutral',
+      label: 'Neutral'
+    },
+    {
+      name: 'gray',
+      label: 'Gray'
+    },
+    {
+      name: 'zinc',
+      label: 'Zinc'
+    },
+    {
+      name: 'stone',
+      label: 'Stone'
+    },
+    {
+      name: 'slate',
+      label: 'Slate'
+    }
+  ]
+}
