e35cc3b3c1b9de45d5e05f6f6fc4b33673f236a8
wildduck <wezonaser50@gmail.com>
2024-12-10 12:00:00 +0200

    feat: new varients in the button with new border styles

---
+++ apps/www/ui/registry/registry-examples-components/button/button-27.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Button } from '@/registry/registry-ui-components/button'
+
+export default function Button26Demo() {
+  return (
+    <div className="block">
+      <Button
+        aria-label="Inbox button"
+        type="button"
+        role="button"
+        variant="muted"
+      >
+        Button
+      </Button>
+    </div>
+  )
+}
