43bdba419967eb630683b096c3e5dba623d47d3d
abdo-spices <spices.digital@gmail.com>
2025-03-06 20:23:47 +0200

    feat(docs): improve sidebar navigation with collapsible items

--- apps/docs/app/(app)/docs/layout.tsx
+++ apps/docs/app/(app)/docs/layout.tsx
@@ -15,14 +15,12 @@ export default function DocsLayout({ children }: DocsLayoutProps) {
   //   </div>
   // )
   return (
-    <div className="">
-      <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10 max-w-screen-2xl">
-        <aside className="fixed show-scroll-hover border-r top-14 z-30 -ml-2 hidden w-full h-[calc(100vh-3.5rem)] px-8 pb-8 shrink-0 md:sticky md:block">
-          <DocsSidebarNav config={docsConfig} />
-          <DocsSidebarNav config={docsConfig} />
-        </aside>
-        {children}
-      </div>
+    <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10 max-w-screen-2xl">
+      <aside className="fixed show-scroll-hover border-r top-14 z-30 -ml-2 hidden w-full h-[calc(100vh-3.5rem)] px-8 pb-8 shrink-0 md:sticky md:block">
+        <DocsSidebarNav config={docsConfig} />
+        <DocsSidebarNav config={docsConfig} />
+      </aside>
+      {children}
     </div>
   )
 }
--- apps/docs/components/docs-side-bar.tsx
+++ apps/docs/components/docs-side-bar.tsx
@@ -4,8 +4,10 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { SidebarNavItem } from 'types/nav'
 
-import { type DocsConfig } from '~/config/docs'
 import { cn } from '@duck/libs/cn'
+import { ChevronDown } from 'lucide-react'
+import { useState } from 'react'
+import { type DocsConfig } from '~/config/docs'
 
 export interface DocsSidebarNavProps {
   config: DocsConfig
@@ -13,7 +15,7 @@ export interface DocsSidebarNavProps {
 
 export function DocsSidebarNav({ config }: DocsSidebarNavProps) {
   const pathname = usePathname()
-
+  const [isOpen, SetIsOpen] = useState(false)
   const items = pathname?.startsWith('/charts')
     ? config.chartsNav
     : config.sidebarNav
@@ -21,12 +23,20 @@ export function DocsSidebarNav({ config }: DocsSidebarNavProps) {
   return items.length ? (
     <div className="w-full">
       {items.map((item, index) => (
-        <div key={index} className={cn('pb-4')}>
-          <h4 className="mb-1 rounded-md px-2 py-1 text-sm font-semibold">
+        <div key={index}>
+          <button
+            className="mb-1 flex justify-between cursor-pointer rounded-md w-full text-start px-2 py-1 text-sm font-semibold"
+            onClick={() => SetIsOpen(!isOpen)}
+          >
             {item.title}
-          </h4>
+            <ChevronDown />
+          </button>
           {item?.items?.length && (
-            <DocsSidebarNavItems items={item.items} pathname={pathname} />
+            <DocsSidebarNavItems
+              className={isOpen ? '!grid-rows-[1fr]' : ''}
+              items={item.items}
+              pathname={pathname}
+            />
           )}
         </div>
       ))}
@@ -37,53 +47,64 @@ export function DocsSidebarNav({ config }: DocsSidebarNavProps) {
 interface DocsSidebarNavItemsProps {
   items: SidebarNavItem[]
   pathname: string | null
+  className?: string
 }
 
 export function DocsSidebarNavItems({
   items,
   pathname,
+  className,
 }: DocsSidebarNavItemsProps) {
   return items?.length ? (
-    <div className="grid grid-flow-row auto-rows-max text-sm">
-      {items.map((item, index) =>
-        item.href && !item.disabled ? (
-          <Link
-            key={index}
-            href={item.href}
-            className={cn(
-              'group flex w-full items-center rounded-md border border-transparent px-2 py-1 hover:underline',
-              item.disabled && 'cursor-not-allowed opacity-60',
-              pathname === item.href
-                ? 'font-medium text-foreground'
-                : 'text-muted-foreground',
-            )}
-            target={item.external ? '_blank' : ''}
-            rel={item.external ? 'noreferrer' : ''}
-          >
-            {item.title}
-            {item.label && (
-              <span className="ml-2 rounded-md bg-[#89b4fa] px-1.5 py-0.5 text-xs leading-none text-[#000000] no-underline group-hover:no-underline">
-                {item.label}
-              </span>
-            )}
-          </Link>
-        ) : (
-          <span
-            key={index}
-            className={cn(
-              'flex w-full cursor-not-allowed items-center rounded-md p-2 text-muted-foreground hover:underline',
-              item.disabled && 'cursor-not-allowed opacity-60',
-            )}
-          >
-            {item.title}
-            {item.label && (
-              <span className="ml-2 rounded-md bg-muted px-1.5 py-0.5 text-xs leading-none text-muted-foreground no-underline group-hover:no-underline">
-                {item.label}
-              </span>
-            )}
-          </span>
-        ),
+    <div
+      className={cn(
+        'grid text-sm  transition-all grid-rows-[0fr] will-change-[grid-template-rows]',
+        className,
       )}
+    >
+      <ul className={cn('overflow-hidden  transition-all ')}>
+        {items.map((item, index) =>
+          item.href && !item.disabled ? (
+            <li>
+              <Link
+                key={index}
+                href={item.href}
+                className={cn(
+                  'group flex w-full items-center rounded-md border border-transparent px-2 py-1 hover:underline',
+                  item.disabled && 'cursor-not-allowed opacity-60',
+                  pathname === item.href
+                    ? 'font-medium text-foreground'
+                    : 'text-muted-foreground',
+                )}
+                target={item.external ? '_blank' : ''}
+                rel={item.external ? 'noreferrer' : ''}
+              >
+                {item.title}
+                {item.label && (
+                  <span className="ml-2 rounded-md bg-[#89b4fa] px-1.5 py-0.5 text-xs leading-none text-[#000000] no-underline group-hover:no-underline">
+                    {item.label}
+                  </span>
+                )}
+              </Link>
+            </li>
+          ) : (
+            <span
+              key={index}
+              className={cn(
+                'flex w-full cursor-not-allowed items-center rounded-md p-2 text-muted-foreground hover:underline',
+                item.disabled && 'cursor-not-allowed opacity-60',
+              )}
+            >
+              {item.title}
+              {item.label && (
+                <span className="ml-2 rounded-md bg-muted px-1.5 py-0.5 text-xs leading-none text-muted-foreground no-underline group-hover:no-underline">
+                  {item.label}
+                </span>
+              )}
+            </span>
+          ),
+        )}
+      </ul>
     </div>
   ) : null
 }
