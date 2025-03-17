43e7231ddb7d44c6da09d500826f00791707e12c
abdo-spices <spices.digital@gmail.com>
2025-03-17 21:08:53 +0200

    refactor(benchmark): remove unused images and upgrade to tailwind 4

--- packages/benchmark/vite.config.ts
+++ packages/benchmark/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import path from "node:path"
+import tailwindcss from '@tailwindcss/vite'
 
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [ react(),tailwindcss(),],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
