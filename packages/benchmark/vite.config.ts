8fb4eae044a4ff7e3283dfc88808a6da21d60949
abdo-spices <spices.digital@gmail.com>
2025-03-18 23:08:21 +0200

    feat(benchmark): add vite-bundle-analyzer and vite-plugin-inspect, update dependencies

--- packages/benchmark/vite.config.ts
+++ packages/benchmark/vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
-
 import react from '@vitejs/plugin-react-swc'
+import { analyzer } from 'vite-bundle-analyzer'
+import Inspect from 'vite-plugin-inspect'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), analyzer({
+    openAnalyzer: true,
+  })
+    , Inspect()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
