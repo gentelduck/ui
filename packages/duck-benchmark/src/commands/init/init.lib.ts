7a5493e13e3b96d7859b6ae737caa3506df5db32
wildduck2 <wezonaser50@gmail.com>
2025-03-21 07:53:15 +0200

    feat(benchmark): still working on the render

--- packages/duck-benchmark/src/commands/init/init.lib.ts
+++ packages/duck-benchmark/src/commands/init/init.lib.ts
@@ -1,15 +1,16 @@
-import path, { resolve } from 'node:path'
-import fs from 'fs-extra'
+import path from 'node:path'
 import { init_options_schema, InitOptions } from './init.dto'
 import { get_project_config } from '~/src/utils'
 import { list_files } from '~/src/utils/list-files'
-import { execa } from 'execa'
-import { FileInfo, FolderInfo } from '~/src/utils/list-files/list-files.dto'
 import { spinner as Spinner } from '~/src/utils/spinner'
-import { compileBenchmark } from '~/src/utils/compile-benchmark'
+
+import {
+  compile_benchmark,
+  render_benchmark,
+} from '~/src/utils/compile-benchmark'
 
 export async function init_command_action(opt: InitOptions) {
-  const spinner = Spinner('Initializing...').start()
+  const spinner = Spinner('Initializing...')
   const options = init_options_schema.parse(opt)
   const cwd = path.resolve(options.cwd)
 
@@ -19,15 +20,16 @@ export async function init_command_action(opt: InitOptions) {
 
   const entries_json = await list_files({
     cwds: [config.src],
-    filter: ['', 'vite-env.d.ts', 'main.tsx'],
+    filter: ['button', 'vite-env.d.ts', 'main.tsx'],
     spinner,
   })
-  const rs = await compileBenchmark({ folders: entries_json })
-  // console.dir(entries_json, { depth: 11 })
+  await compile_benchmark({ folders: entries_json, spinner, cwd })
+  await render_benchmark({ folders: entries_json, spinner, cwd })
 
   //TODO: make sure to compile each file in these folders.
   //TODO: get compile result and statics regarding each file in the folder.
   // logger.info({ args: ['Done.!'] })
 
-  spinner.succeed('Done!')
+  // spinner.succeed('Done!')
+  console.dir(entries_json, { depth: 11 })
 }
--- packages/duck-benchmark/src/utils/compile-benchmark/compile-benchmark.types.ts
+++ packages/duck-benchmark/src/utils/compile-benchmark/compile-benchmark.types.ts
@@ -1,6 +1,18 @@
-import { FolderInfo } from '../list-files'
+import { Ora } from 'ora'
+import { FileInfo, FolderInfo } from '../list-files'
 
 export type CompileBenchmarkParams = {
   folders: FolderInfo[]
   visited?: Set<string>
+  spinner: Ora
+  cwd: string
+}
+
+export type CompileFileParams = { file: FileInfo; spinner: Ora; cwd: string }
+
+export type RenderBenchmarkParams = {
+  folders: FolderInfo[]
+  visited?: Set<string>
+  spinner: Ora
+  cwd: string
 }
