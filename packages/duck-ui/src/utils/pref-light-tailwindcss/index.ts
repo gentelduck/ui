5a4395ed7722c3038eca31b2ef585ab68e3fc4de
wildduck2 <wezonaser50@gmail.com>
2025-03-22 05:47:15 +0200

    fixing

--- packages/duck-ui/src/utils/pref-light-tailwindcss/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './pref-light-tailwindcss'
-export * from './pref-light-tailwindcss.dto'
-export * from './pref-light-tailwindcss.constants'
-export * from './pref-light-tailwindcss.lib'
