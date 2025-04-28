1dd417f8ad4298d87c90068383cff22f24a72992
abdo-spices <spices.digital@gmail.com>
2025-04-28 06:39:58 +0300

    chore(aria-feather): init build

---
+++ packages/aria-feather/index.ts
@@ -0,0 +1,3 @@
+import { Button } from './src/dialog/core'
+
+export { Button }
---
+++ packages/aria-feather/src/dialog/core.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react"
+
+function Button(): React.JSX.Element {
+  return (
+    <>
+      <button></button>
+    </>
+  )
+}
+
+export { Button }
