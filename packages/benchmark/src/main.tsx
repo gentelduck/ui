ec7dfb67bc6fe0fd5a5debd565280560e8755aa9
abdo-spices <spices.digital@gmail.com>
2025-04-28 09:48:27 +0300

    fix(benchmarking): build works

--- packages/benchmark/src/main.tsx
+++ packages/benchmark/src/main.tsx
@@ -13,4 +13,3 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
- 
--- packages/benchmark/vite.config.ts
+++ packages/benchmark/vite.config.ts
@@ -10,7 +10,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   build: {
     sourcemap: true,
-    minify: false
   },
   plugins: [react(), tailwindcss(),
   ],
