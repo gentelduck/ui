fd164b77b477599ab2297efc6e15d6f411954032
abdo-spices <spices.digital@gmail.com>
2025-05-04 20:45:10 +0300

    refactor(drawer): add snap point to DrawerContent for improved user interaction and styling consistency

--- packages/registry-ui-duckui/src/experimental/drawer/drawer.tsx
+++ packages/registry-ui-duckui/src/experimental/drawer/drawer.tsx
@@ -61,6 +61,8 @@ const DrawerContent = ({
   const { open, ref, onOpenChange } = useDialogContext()
   const [shouldRender] = useShouldRender(open, renderOnce ?? false)
   const [closeOverlay] = useOverlayClose()
+  const snapPoint = React.useRef<HTMLSpanElement>(null)
+
 
   React.useEffect(() => {
     if (!open) {
@@ -76,31 +78,29 @@ const DrawerContent = ({
   }, [open])
 
   return (
-    <>
-      <dialog
-        ref={ref}
-        className={cn('border border-border w-full max-w-full rounded-lg bg-background p-0 m-0 inset-unset shadow-sm',
-          AnimVariants(), AnimDrawerVariants({ side: side, }), className)}
-        onClick={closeOverlay}
-        {...props}
-      >
-        {shouldRender && (
-          <div className='p-6 w-full h-full'>
-            <span className='flex w-full justify-center'>
-              <span className='bg-border w-1/6 h-3 rounded-full'></span>
-            </span>
-            <button
-              aria-label='close'
-              className='absolute right-4 top-4 size-4 cursor-pointer opacity-70 rounded hover:opacity-100 transition-all'
-              onClick={() => onOpenChange(false)}
-            >
-              <X aria-hidden size={20} />
-            </button>
-            {children}
-          </div>
-        )}
-      </dialog>
-    </>
+    <dialog
+      ref={ref}
+      className={cn('border border-border w-full max-w-full rounded-lg bg-background p-0 m-0 inset-unset shadow-sm',
+        AnimVariants(), AnimDrawerVariants({ side: side, }), className)}
+      onClick={closeOverlay}
+      {...props}
+    >
+      {shouldRender && (
+        <div className='p-6 w-full h-full select-none'>
+          <span className='flex w-full justify-center'>
+            <span ref={snapPoint} className='bg-border w-1/6 h-3 rounded-full cursor-grab active:cursor-grabbing'></span>
+          </span>
+          <button
+            aria-label='close'
+            className='absolute right-4 top-4 size-4 cursor-pointer opacity-70 rounded hover:opacity-100 transition-all'
+            onClick={() => onOpenChange(false)}
+          >
+            <X aria-hidden size={20} />
+          </button>
+          {children}
+        </div>
+      )}
+    </dialog>
   )
 }
 
