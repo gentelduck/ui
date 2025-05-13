9971e30df0dc73ed641006c4b566864217f29f3b
abdo-spices <spices.digital@gmail.com>
2025-05-13 15:56:06 +0300

    feat(website): change router to tanstack start

---
+++ apps/website/app/routes/__root.tsx
@@ -0,0 +1,64 @@
+// import { Outlet, createRootRoute } from '@tanstack/react-router'
+// import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+
+// import Header from '../components/Header'
+
+// export const Route = createRootRoute({
+//   component: () => (
+//     <>
+//       <Header />
+
+//       <Outlet />
+//       <TanStackRouterDevtools />
+//     </>
+//   ),
+// })
+
+// app/routes/__root.tsx
+import type { ReactNode } from 'react'
+import {
+  Outlet,
+  createRootRoute,
+  HeadContent,
+  Scripts,
+} from '@tanstack/react-router'
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: 'TanStack Start Starter',
+      },
+    ],
+  }),
+  component: RootComponent,
+})
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  )
+}
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html>
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
+    </html>
+  )
+}
