6db4778db8eab35fbe459e5b732ce0f039e3808e
wildduck2 <wezonaser50@gmail.com>
2025-05-29 12:00:00 +0300

    feat(registry): working on the dropdown menu

---
+++ packages/registry-ui-duckui/src/dropdown-menu/_dropdown-menu/dropdown-menu-wrapper.tsx
@@ -0,0 +1,313 @@
+import { cn } from '@gentleduck/libs/cn'
+import { groupArrays } from '@gentleduck/libs/group-array'
+import * as React from 'react'
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from './dropdown-menu'
+import { Button, ButtonProps, buttonVariants, CommandType } from '../../button'
+
+export interface DropdownMenuOptionsDataType
+  extends Partial<
+    React.ComponentPropsWithoutRef<typeof DropdownMenuCheckboxItem> &
+    React.ComponentPropsWithoutRef<typeof DropdownMenuItem> &
+    React.ComponentPropsWithoutRef<typeof DropdownMenuRadioItem>
+  > {
+  actionType: 'drawer' | 'dialog' | 'sheet' | 'item'
+  command?: React.ComponentPropsWithoutRef<typeof DropdownMenuShortcut> & CommandType
+  icon?: React.ReactNode
+  nestedData?: React.ComponentPropsWithoutRef<typeof DropdownMenuSubContent> & DropdownMenuOptionsType
+}
+
+export interface DropdownMenuOptionsType {
+  itemType: 'checkbox' | 'radio' | 'label'
+  optionsData: DropdownMenuOptionsDataType[]
+  group?: number[]
+}
+
+export interface DropdownMenuViewProps extends React.ComponentPropsWithoutRef<typeof DropdownMenu> {
+  trigger: React.ComponentPropsWithoutRef<typeof DropdownMenuTrigger> & ButtonProps
+  content: {
+    label?: React.ComponentPropsWithoutRef<typeof DropdownMenuLabel>
+    options: DropdownMenuOptionsType
+  } & React.ComponentPropsWithoutRef<typeof DropdownMenuContent>
+}
+
+export type ItemActionOpenState = {
+  id: `${DropdownMenuOptionsDataType['actionType']}-${number}`
+  value: boolean
+}
+export interface DropdownMenuContextType {
+  open: ItemActionOpenState | null
+  setOpen: React.Dispatch<React.SetStateAction<ItemActionOpenState | null>>
+}
+
+export const DropdownMenuContext = React.createContext<DropdownMenuContextType | null>(null)
+
+export const useDropdownMenuContext = () => {
+  const context = React.useContext(DropdownMenuContext)
+  if (!context) {
+    throw new Error('useDropdownMenuContext must be used within a DropdownMenuContextProvider')
+  }
+  return context
+}
+
+export const DropdownMenuProvider = ({
+  children,
+}: {
+  children: React.ReactNode
+}) => {
+  const [open, setOpen] = React.useState<ItemActionOpenState | null>(null)
+  return <DropdownMenuContext.Provider value={{ open, setOpen }}>{children}</DropdownMenuContext.Provider>
+}
+
+export function DropdownMenuView({ content, trigger, ...props }: DropdownMenuViewProps) {
+  const { className: optionsClassName, options, label, align = 'end', ...contentProps } = content ?? {}
+
+  const groupedOption = groupArrays(options?.group ?? [options?.optionsData?.length || 1], options?.optionsData ?? [])
+
+  return (
+    <DropdownMenu {...props}>
+      <DropdownWrapperTrigger trigger={trigger} />
+
+      <DropdownMenuContent align={align} className={cn('w-[200px]', optionsClassName)} {...contentProps}>
+        <DropdownWrapperLabel label={label} />
+
+        {groupedOption.map((group, idx) => {
+          return (
+            <React.Fragment key={`group-${idx}`}>
+              {group.map((item, idx) => {
+                const { nestedData, key: _key } = item
+
+                const {
+                  className: nestedClassName,
+                  group: nestedGroup,
+                  optionsData: nestedOptions,
+                  ...nestedProps
+                } = nestedData ?? {}
+
+                const groupedNestedOption =
+                  groupArrays(
+                    nestedData?.group ?? [nestedData?.optionsData?.length || 1],
+                    nestedData?.optionsData ?? [],
+                  ) ?? []
+
+                return (
+                  <React.Fragment key={`item-${idx}`}>
+                    {nestedData?.optionsData?.length ? (
+                      <DropdownMenuSub key={`sub-item-${idx}`}>
+                        <DropdownWrapperSubTrigger trigger={item} />
+                        <DropdownMenuPortal>
+                          <DropdownMenuSubContent className={cn('w-[200px]', nestedClassName)} {...nestedProps}>
+                            {groupedNestedOption?.map((nestedItem, idx) => {
+                              return (
+                                <React.Fragment key={`nested-${idx}`}>
+                                  <DropdownWrapperSubContent itemType={nestedData?.itemType} nestedData={nestedItem} />
+                                  {idx !== groupedNestedOption?.length - 1 && (
+                                    <DropdownMenuSeparator key={`separator-${idx}`} />
+                                  )}
+                                </React.Fragment>
+                              )
+                            })}
+                          </DropdownMenuSubContent>
+                        </DropdownMenuPortal>
+                      </DropdownMenuSub>
+                    ) : (
+                      <DropdownWrapperContentItem idx={idx} item={item} itemType={options.itemType} />
+                    )}
+                  </React.Fragment>
+                )
+              })}
+              {idx !== groupedOption.length - 1 && <DropdownMenuSeparator />}
+            </React.Fragment>
+          )
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export function DropdownWrapperTrigger({
+  trigger,
+}: {
+  trigger: React.ComponentPropsWithoutRef<typeof DropdownMenuTrigger> & ButtonProps
+}) {
+  return (
+    <DropdownMenuTrigger asChild>
+      <Button variant="ghost" size="sm" {...trigger} />
+    </DropdownMenuTrigger>
+  )
+}
+
+export function DropdownWrapperLabel({
+  label,
+}: {
+  label?: React.ComponentProps<typeof DropdownMenuLabel>
+}) {
+  const { className, ...props } = label ?? {}
+  return (
+    label && (
+      <>
+        <DropdownMenuLabel className={cn('text-sm', className)} {...props} />
+        <DropdownMenuSeparator />
+      </>
+    )
+  )
+}
+
+export function DropdownWrapperContentItem({
+  item,
+  itemType,
+  idx,
+}: {
+  item: DropdownMenuOptionsDataType
+  itemType: 'checkbox' | 'radio' | 'label'
+  idx: number
+}) {
+  const {
+    children,
+    className,
+    value,
+    nestedData,
+    actionType,
+    key: _key,
+    onClick,
+    //FIX: removed this hence when i clicked on the item it is always focusing.
+    autoFocus,
+    ...props
+  } = item
+
+  const { setOpen } = useDropdownMenuContext()
+
+  const Component =
+    itemType === 'checkbox' ? DropdownMenuCheckboxItem : itemType === 'radio' ? DropdownMenuRadioItem : DropdownMenuItem
+
+  return (
+    <Component
+      value={value as string}
+      className={cn(
+        buttonVariants({
+          variant: 'ghost',
+          size: 'sm',
+          className: 'flex gap-2 items-center justify-start px-2',
+        }),
+        itemType === 'checkbox' && 'pl-8',
+        itemType === 'radio' && '[&_span_svg]:w-[.5rem] pl-8',
+        className,
+      )}
+      onClick={(e) => {
+        onClick?.(e)
+        setOpen({
+          id: `${actionType}-${idx}`,
+          value: true,
+        })
+      }}
+      {...props}>
+      {item.icon && item.icon}
+      {children}
+      {item.command?.label && (
+        <>
+          <DropdownMenuShortcut {...item.command} children={item.command.children} />
+          <Button
+            className="sr-only hidden"
+            command={{
+              ...item.command,
+              action: () => {
+                setOpen({
+                  id: `${actionType}-${idx}`,
+                  value: true,
+                })
+                item.command?.action?.()
+              },
+            }}
+          />
+        </>
+      )}
+    </Component>
+  )
+}
+
+export function DropdownWrapperSubTrigger({
+  trigger,
+}: {
+  trigger: DropdownMenuOptionsDataType
+}) {
+  const { children, className, icon, ...props } = trigger
+  return (
+    // @ts-ignore
+    <DropdownMenuSubTrigger
+      className={cn(
+        buttonVariants({
+          variant: 'ghost',
+          size: 'sm',
+          className: 'w-full gap-2 px-2',
+        }),
+      )}
+      {...props}>
+      {icon}
+      <span>{children}</span>
+    </DropdownMenuSubTrigger>
+  )
+}
+
+export function DropdownWrapperSubContent({
+  nestedData,
+  itemType,
+}: {
+  nestedData: DropdownMenuOptionsDataType[]
+  itemType: 'checkbox' | 'radio' | 'label'
+}) {
+  const Children = nestedData.map((item, idx) => {
+    const { children, value, icon: Icon, className, ...props } = item
+    const { children: commandChildren, ...commandProps } = item.command ?? {}
+
+    const Component =
+      itemType === 'checkbox'
+        ? DropdownMenuCheckboxItem
+        : itemType === 'radio'
+          ? DropdownMenuRadioItem
+          : DropdownMenuItem
+    console.log(value)
+
+    return (
+      <Component
+        value={value as string}
+        checked={true}
+        key={`nested-item-${idx}`}
+        className={cn(
+          buttonVariants({
+            variant: 'ghost',
+            size: 'sm',
+          }),
+          'flex gap-2 items-center justify-start',
+          itemType !== 'label' && 'pl-8',
+          itemType === 'radio' && '[&_span_svg]:w-[.5rem]',
+          className,
+        )}
+        {...props}>
+        {Icon && Icon}
+        {children}
+        {item.command && (
+          <>
+            <DropdownMenuShortcut children={commandChildren} {...commandProps} key={`nested-item-shortcut-${idx}`} />
+            <Button command={item.command} className="sr-only hidden" />
+          </>
+        )}
+      </Component>
+    )
+  })
+
+  return itemType === 'radio' ? <DropdownMenuRadioGroup value="date">{Children}</DropdownMenuRadioGroup> : Children
+}
---
+++ packages/registry-ui-duckui/src/dropdown-menu/dropdown-menu.hooks.ts
@@ -0,0 +1,54 @@
+import React from 'react'
+import { DropdownMenuContext } from './dropdown-menu'
+import { styleItem } from '../command/command.libs'
+
+export const useDropdownMenuContext = () => {
+  const context = React.useContext(DropdownMenuContext)
+  if (!context) {
+    throw new Error('useDropdownMenuContext must be used within a DropdownMenu')
+  }
+  return context
+}
+
+export function useDropdownMenuActions() {
+  const wrapperRef = React.useRef<HTMLDivElement>(null)
+  const triggerRef = React.useRef<HTMLButtonElement>(null)
+  const contentRef = React.useRef<HTMLDivElement>(null)
+  const overlayRef = React.useRef<HTMLDivElement>(null)
+  const groupsRef = React.useRef<HTMLDivElement[]>([])
+  const itemsRef = React.useRef<HTMLLIElement[]>([])
+  const selectedItemRef = React.useRef<HTMLLIElement | null>(null)
+
+  React.useEffect(() => {
+    triggerRef.current?.addEventListener('click', () => {
+      const open = contentRef.current?.getAttribute('data-open') === 'true'
+      if (!groupsRef.current.length || !itemsRef.current.length) {
+        const groups = wrapperRef.current?.querySelectorAll('[duck-dropdown-menu-group]')
+        const items = wrapperRef.current?.querySelectorAll('[duck-dropdown-menu-item]') as never as HTMLLIElement[]
+        groupsRef.current = Array.from(groups ?? []) as HTMLDivElement[]
+        itemsRef.current = Array.from(items ?? []) as HTMLLIElement[]
+
+        styleItem((itemsRef.current?.[0] as HTMLLIElement) ?? null)
+        itemsRef.current?.[0]?.focus()
+        selectedItemRef.current = (itemsRef.current?.[0] as HTMLLIElement) ?? null
+      }
+
+      contentRef.current?.setAttribute('data-open', String(!open))
+    })
+  }, [])
+
+  //TODO: clear the listeners
+  //TODO: handle the close
+  // overlayRef.current?.addEventListener('click', () => {
+  //   contentRef.current?.setAttribute('data-open', 'false')
+  // })
+  return {
+    wrapperRef,
+    triggerRef,
+    contentRef,
+    overlayRef,
+    groupsRef,
+    itemsRef,
+    selectedItemRef,
+  }
+}
