4bfe2ee1b49f76c10afa1cb64efe6a0e91791519
abdo-spices <spices.digital@gmail.com>
2025-06-04 03:54:04 +0300

    chore: update lockfile, remove benchmark missed merge folders

--- packages/benchmark/src/duck/toggle/duck.tsx
+++
@@ -1,7 +0,0 @@
-import { Bold } from 'lucide-react'
-
-import { Toggle } from '@gentleduck/registry-ui-duckui/toggle'
-
-export default function ToggleDemo() {
-  return <Toggle aria-label="Toggle bold">{/* <Bold className="h-4 w-4" /> */}</Toggle>
-}
